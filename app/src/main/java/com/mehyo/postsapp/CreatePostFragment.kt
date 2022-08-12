a25d64ba0125069a9bad3bdd4b5bf5d7ff36a5b3
Mohyddine <me7yo93@gmail.com>
2022-08-12 15:13:01 +0300

    setup bottom sheet layout

--- app/src/main/java/com/mehyo/postsapp/CreatePostFragment.kt
+++ app/src/main/java/com/mehyo/postsapp/CreatePostFragment.kt
@@ -4,31 +4,24 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.Fragment
-import androidx.navigation.fragment.findNavController
-import com.mehyo.postsapp.databinding.FragmentHomeBinding
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import com.mehyo.postsapp.databinding.FragmentCreatePostBinding
 
-class HomeFragment : Fragment() {
+class CreatePostFragment : BottomSheetDialogFragment() {
 
-    private var _binding: FragmentHomeBinding? = null
+    private var _binding: FragmentCreatePostBinding? = null
     private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        _binding = FragmentCreatePostBinding.inflate(inflater, container, false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        with(binding) {
-            fabAdd.setOnClickListener {
-                findNavController().navigate(R.id.action_homeFragment_to_createPostFragment)
-            }
-        }
     }
 
     override fun onDestroy() {
